b05e816e48ac5d527221f9f0230fcc44b0b89588
anisrashidov <anisjon.rashidov@gmail.com>
2024-08-01 19:03:40 +0500

    deployment

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/anisrashidov/todoAPP/router"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 	fmt.Println("Welcome to your TODO list")
 	log.Fatal(http.ListenAndServe(":8000", router.Router()))
-	fmt.Println("Listening on 127.0.0.1:8000")
+	fmt.Println("Listening on 127.0.0.1:" + port)
 }
