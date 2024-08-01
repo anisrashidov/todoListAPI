e4e25add46a68b76c2e2f8eb957f2dee9477dcf1
anisrashidov <anisjon.rashidov@gmail.com>
2024-08-01 23:01:10 +0500

    hope

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = ":8000"
+		port = "8000"
 	}
 	fmt.Println("Welcome to your TODO list")
-	fmt.Println("Listening on 127.0.0.1" + port)
-	log.Fatal(http.ListenAndServe(port, router.Router()))
+	fmt.Println("Listening on 127.0.0.1:" + port)
+	log.Fatal(http.ListenAndServe(":"+port, router.Router()))
 }
